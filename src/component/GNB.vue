a2169df4c51fb85eb00c0b3ac2835ccb80643ce8
daeun lee <167169154+delee-dix@users.noreply.github.com>
2024-12-16 18:30:59 +0900

    [edit] GNB
    GNB 영역 가이드라인

--- src/component/GNB.vue
+++ src/component/GNB.vue
@@ -1,14 +1,40 @@
 <template>
-    <div class="gnb">GNB</div>
+    <div class="gnb_container">
+        <div class="condition_left">
+            (car) < 24.5℃ > (seat)
+        </div>
+        <div class="gnb_icon">
+            gnb1 | gnb2 | gnb3 | gnb4
+        </div>
+        <div class="condition_right">
+            (front) (rear) < 24.5℃ > (seat)
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
-.gnb {
+.gnb_container {
     display: flex;
+    padding: 10px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+}
+
+.condition_left
+.gnb_icon
+.condition_right {
+    display: flex;
+    justify-content: center;
     align-items: center;
     position: relative;
     bottom: 0;
+
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: cover;    /* contain? */
 }
 </style>
