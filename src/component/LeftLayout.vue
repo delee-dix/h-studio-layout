7be55ee6434717fac91f05012710ddfd117780cb
daeun lee <167169154+delee-dix@users.noreply.github.com>
2024-12-26 09:48:14 +0900

    Update LeftLayout.vue

--- src/component/LeftLayout.vue
+++ src/component/LeftLayout.vue
@@ -20,13 +20,56 @@ defineProps<IProps>();
     max-width: 100%;
     max-height: 100%;
     height: 100%;
-    object-fit: cover;    /* contain? */
+    object-fit: cover;
     border: 5px solid;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
+
     img {
         width: 100%;
         height: 100%;
+    }
+
+    .ottScreen {
+        background-color: black;
+        display: flex;
+        justify-items: center;
+        align-items: center;
+        object-fit: contain;
+        height: 65vh;
+    }
+
+    .dataScreen {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 10px;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        padding: 5vh;
+    }
+
+    .title {
+        font-size: 50px;
+        color: blue;
+        padding: 20px;
+    }
+
+    .image-container {
+        display: flex;
+        gap: 20px;
+    }
+
+    .leftInfo,
+    .rightInfo {
+        display: flex;
+        flex-direction: column;
+    }
+
+    img {
+        width: 100%;
+        height: auto;
+        /* max-width: 200px; */
     }
 
 }
