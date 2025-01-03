ee280df57ce22fc1e81af0e1e006e230b4c9340f
daeun lee <dix.delee@gmail.com>
2025-01-03 12:05:27 +0900

    [edit] button position
    각각의 버튼의 위치를 지정하는 작업 (진행 중)

---
+++ src/component/Button.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="button" @click="$emit('click')">
+        {{ label }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps, defineEmits } from 'vue';
+
+defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+});
+
+defineEmits(['click']);
+
+// const buttonPosition = ref({ top: "67%", left: "26%" });
+
+// const updateButtonPosition = (index1: number) => {
+//     if (index1 === 4) {
+//         buttonPosition.value = { top: "40%", left: "27%" };
+//     } else if (index1 === 6) {
+//         buttonPosition.value = { top: "35%", left: "26%" };
+//     } else {
+//         buttonPosition.value = { top: "67%", left: "26%" };
+//     }
+//     console.log("Button position updated:", buttonPosition.value);
+// };
+</script>
+
+<style scoped>
+.button {
+    position: absolute;
+    width: 360px;
+    height: 70px;
+    /* top: 68.7%;
+    left: 32.1%; */
+    transform: translate(-25%, 425%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 23px 140px;
+    font-size: 20px;
+    cursor: pointer;
+    /* background-color: rgba(31, 90, 235, 1); */
+    background-color: lightcoral;
+    color: white;
+    border-radius: 10px;
+    border: none;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+}
+
+.button:hover {
+    background-color: rgba(31, 90, 255, 0.9);
+}
+</style>
---
+++ src/component/FullLayout.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="fullLayout">
+        <img :src="imagePath" alt="Sample Image" />
+    </div>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+    imagePath?: string | undefined;
+    backgroundImage?: string;
+}
+defineProps<IProps>();
+</script>
+
+<style scoped>
+.fullLayout {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    max-width: 100%;
+    max-height: 100%;
+    height: 100%;
+    border: 5px solid;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    background-repeat: no-repeat;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
