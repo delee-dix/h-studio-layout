a35925ad8ce04e2e9d6b4e46ae782964ea5d663a
bwlee-dix <bwlee@dixsoft.kr>
2025-01-06 18:27:23 +0900

    feat: RightLayout in First View

---
+++ src/component/icon/WhetherSmallIcon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps, computed } from 'vue';
+
+
+type IconName = 'sun' | 'sunset' | 'moon';
+
+const props = defineProps<{
+  iconName: IconName;
+}>();
+
+const iconPath = computed(() => `/src/asset/ic-whether-${props.iconName}.svg`);
+</script>
+
+<template>
+  <img :src="iconPath" alt="" width="32" height="32" />
+</template>
