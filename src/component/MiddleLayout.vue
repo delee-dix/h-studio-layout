74d5b81ae1ba82c7068b0a9c6488d5b09b5b8eb1
daeun lee <dix.delee@gmail.com>
2025-01-06 19:40:28 +0900

    [add] RouteModal
    • 버튼을 누르면 다음 로직으로 변경
    • 지도에 있는 포인트를 선택하면 확대 (구현 진행 중)

--- src/component/MiddleLayout.vue
+++ src/component/MiddleLayout.vue
@@ -1,10 +1,48 @@
 <template>
     <div class="middleLayout">
-        <img :src="imagePath" alt="Sample Image" />
+        <img :src="imagePath" alt="Sample Image" ref="mapImage" class="map" />
+        <RouteModal />
+        <div class="points">
+            <button v-for="point in points" :key="point.id" :style="getButtonStyle(point)" @click="handleClick('Point')">
+                ★
+            </button>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import RouteModal from './routeModal.vue';
+
+import { ref, type Ref } from 'vue';
+
+interface Point {
+    id: number;
+    x: number;
+    y: number;
+}
+
+const points = ref<Point[]>([
+    { id: 1, x: 1580, y: 160 },
+    { id: 2, x: 1760, y: 175 },
+    { id: 3, x: 1560, y: 630 },
+]);
+
+const mapImage: Ref<HTMLImageElement | null> = ref(null);
+const scale = ref(1.2);
+
+const getButtonStyle = (point: Point) => {
+    return {
+        position: 'absolute',
+        top: `${point.y}px`,
+        left: `${point.x}px`,
+        transform: `scale(${scale.value})`,
+    } as Record<string, string>;
+};
+
+const handleClick = (point: string) => {
+    alert(`${point} clicked`);
+};
+
 interface IProps {
     imagePath?: string | undefined;
 }
